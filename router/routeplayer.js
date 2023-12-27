e64fa214e1280cc2bd61c7f1c9524cafb52aae40
Kavitha <kavithasundharamoorthy.ss@gmail.com>
2023-12-27 11:59:43 +0530

    created sample.js

--- router/routeplayer.js
+++ router/routeplayer.js
@@ -2,6 +2,10 @@ const express = require('express')
 const Route = express.Router();
 const controller = require('../controller/controlplayer')
 
+Route.route('/').get((req, res) => {
+    res.send('Hello')
+})
+
 Route
     .post('/player', controller.addUser)
     .get('/player', controller.getAllUser)
